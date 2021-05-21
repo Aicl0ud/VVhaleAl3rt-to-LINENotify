d3aafa0bf9f81b8ae1193184861e25f942737b29
Aicl0ud <teerasit.won@student.mahidol.ac.th>
2021-05-22 03:12:45 +0700

    Completed notify WhaleAlert on LINE
    Abandoned using API from WhaleAlert since I got another solution...

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # VVhaleAl3rt-to-LINENotify
-fetching API from WhaleAlert to LINE notify with javascript 
+Retrieve tweets from WhaleAlert to LINE notify with GoogleSheet log
--- whalealert-to-LINE.js
+++ whalealert-to-LINE.js
@@ -1,10 +1,12 @@
 const Bearer = 'Bearer _SECRET_'
-
+const Sheet = SpreadsheetApp.openById('_SheetID_').getSheets()[0]
 function Main() {
 
-    var msg = 'test'
-    sendMessage(msg)
-    // console.log(msg)
+    var lastRow = Sheet.getLastRow()
+    var msg = Sheet.getSheetValues(lastRow, 2, 1, 1) //startRow, startCol, returnRow 'till, returnCol 'till
+    console.log(msg)
+    // sendMessage(msg)
+
 }
 
 function sendMessage(message) {
